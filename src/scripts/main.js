68d97425461bc94ed053a87bda36e827bcb37114
FEHR <fabiohfehr@gmail.com>
2024-04-29 19:46:09 +0100

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    $(document).ready(function() {
+        $('header button').click(function(){
+            $('form').slideDown();
+        })
+    
+        $('#botao-cancelar').click(function(){
+            $('form').slideUp();
+        })
+    })
+    $(document).ready(function(){
+        $('form').on('submit', function(e){
+            e.preventDefault();
+            const nomeDaTarefa = $('input').val();
+            const novaTarefa = $(`<li>${nomeDaTarefa}</li>`)
+    
+            $(novaTarefa).appendTo('ul')
+            $('input').val('')
+        })
+    
+        $('ul').on('click', 'li', function(e){
+            e.preventDefault();
+            $(this).toggleClass('riscarTarefa')
+        })
+    
+    })
+})
+
